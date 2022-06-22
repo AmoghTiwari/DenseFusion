e4389d2e3bde4ffe0ba90344a5bca05f3e12f867
amoghtiwari <amoghtiwari2000@gmail.com>
2022-06-22 11:28:12 +0530

    Added initialization file

---
+++ initialize_densefusion.sh
@@ -0,0 +1,14 @@
+mkdir -p /ssd_scratch/cvit/amoghtiwari/DenseFusion/
+cd /ssd_scratch/cvit/amoghtiwari/DenseFusion/
+mkdir data
+mkdir checkpoints
+
+cd data
+scp amoghtiwari@ada.iiit.ac.in:/share3/amoghtiwari/data/Linemod_preprocessed_for_densefusion.zip ./
+unzip Linemod_preprocessed_for_densefusion.zip
+rm Linemod_preprocessed_for_densefusion.zip
+
+cd ../checkpoints
+scp amoghtiwari@ada.iiit.ac.in:/share3/amoghtiwari/checkpoints/densefusion_trained_checkpoints.zip ./
+unzip densefusion_trained_checkpoints.zip
+rm densefusion_trained_checkpoints.zip
